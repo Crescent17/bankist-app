e65682e7ce2c2b01acf5952624fb242d0d7032f9
Marat <94042173+Crescent17@users.noreply.github.com>
2023-07-27 18:15:00 +0300

    create function to create usernames for users

--- script.js
+++ script.js
@@ -76,16 +76,8 @@ const displayMovements = function(movements) {
 
 
 displayMovements(account1.movements);
-/////////////////////////////////////////////////
-/////////////////////////////////////////////////
-// LECTURES
-
-const currencies = new Map([
-  ['USD', 'United States dollar'],
-  ['EUR', 'Euro'],
-  ['GBP', 'Pound sterling']
-]);
-
-const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
-
-/////////////////////////////////////////////////
+const createUsername = accs => accs.forEach(acc => {
+  acc.username = acc.owner.toLowerCase().split(' ').map(name => name[0]).join('')
+})
+createUsername(accounts);
+console.log(accounts);
